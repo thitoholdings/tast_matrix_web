1ddc20d62564f8c91f9298c1e668fdf94940b84f
Kutlo Sekwamote <32220345+ksekwamote@users.noreply.github.com>
2023-03-09 14:18:17 +0200

    frontend

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useContext, useEffect } from "react";
+import "./App.css";
+import { getMonth } from "./util";
+import CalendarHeader from "./components/CalendarHeader";
+import Sidebar from "./components/Sidebar";
+import Month from "./components/Month";
+import GlobalContext from "./context/GlobalContext";
+import EventModal from "./components/EventModal";
+import axios from "axios";
+function App() {
+  const [currenMonth, setCurrentMonth] = useState(getMonth());
+  const { monthIndex, showEventModal } = useContext(GlobalContext);
+  const context = useContext(GlobalContext);
+
+  useEffect(() => {
+    setCurrentMonth(getMonth(monthIndex));
+  }, [monthIndex]);
+
+  return (
+    <React.Fragment>
+      {showEventModal && <EventModal />}
+
+      <div className="h-screen flex flex-col">
+        <button onClick={() => console.log(context)}>Kindly Press Me</button>
+        <CalendarHeader />
+        <div className="flex flex-1">
+          <Sidebar />
+          <Month month={currenMonth} />
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
